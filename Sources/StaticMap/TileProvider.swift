751d09d3654a248121015cdf9bfa18cbeef9a7f6
fwcd <fwcd@fwcd.dev>
2024-07-05 00:11:42 +0200

    Add some standard tile providers

--- Sources/StaticMap/TileProvider.swift
+++ Sources/StaticMap/TileProvider.swift
@@ -4,7 +4,20 @@ import Foundation
 /// 
 /// See https://wiki.openstreetmap.org/wiki/Raster_tile_providers
 struct TileProvider {
-    static let osm = Self(urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png")
+    /// OpenStreetMap's standard tile layer.
+    static let standard = Self(urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png")
+    /// CyclOSM
+    static let cyclOSM = Self(urlTemplate: "https://a.tile-cyclosm.openstreetmap.fr/[cyclosm|cyclosm-lite]/{z}/{x}/{y}.png")
+    /// OSM Germany
+    static let osmDE = Self(urlTemplate: "https://tile.openstreetmap.de/{z}/{x}/{y}.png")
+    /// OSM France, priority given to French names and symbols
+    static let osmFR = Self(urlTemplate: "https://a.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png")
+    /// Humanitarian map style.
+    static let humanitarian = Self(urlTemplate: "https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png")
+    /// Artistic black and white map. Seems to require authentication.
+    static let stamenToner = Self(urlTemplate: "https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}.png")
+    /// Artistic watercolor-style map. Seems to require authentication.
+    static let stamenWatercolor = Self(urlTemplate: "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg")
 
     /// The template URL for each tile. May use `{x}`, `{y}` and `{z}` as placeholders.
     let urlTemplate: String
