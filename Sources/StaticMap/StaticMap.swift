1a1491f35e2a8d759248ab32911df1ea3d99a4a0
fwcd <fwcd@fwcd.dev>
2024-07-04 22:51:24 +0200

    Add basic StaticMap struct

--- Sources/StaticMap/StaticMap.swift
+++ Sources/StaticMap/StaticMap.swift
@@ -1,2 +1,29 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Utils
+
+public struct StaticMap {
+    public var width: Int
+    public var height: Int
+    public var padding: (Int, Int)
+    public var zoom: UInt8?
+    public var center: GeoCoordinates?
+    public var urlTemplate: String
+    public var tileSize: Int
+
+    public init(
+        width: Int = 300,
+        height: Int = 300,
+        padding: (Int, Int) = (0, 0),
+        zoom: UInt8? = nil,
+        center: GeoCoordinates? = nil,
+        urlTemplate: String = "https://a.tile.osm.org/{z}/{x}/{y}.png",
+        tileSize: Int = 256
+    ) {
+        self.width = width
+        self.height = height
+        self.padding = padding
+        self.zoom = zoom
+        self.center = center
+        self.urlTemplate = urlTemplate
+        self.tileSize = tileSize
+    }
+}
