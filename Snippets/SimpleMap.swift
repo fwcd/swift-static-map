ad66b804fcbedbe2c6c47095fa05777255e37dcc
fwcd <fwcd@fwcd.dev>
2024-09-28 04:56:25 +0200

    Add snippet

---
+++ Snippets/SimpleMap.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Geodesy
+import StaticMap
+
+guard CommandLine.argc == 2 else {
+    print("Usage: \(CommandLine.arguments[0]) <output path>")
+    exit(1)
+}
+
+let outputPath = CommandLine.arguments[1]
+
+let staticMap = StaticMap(center: .init(latitude: 60, longitude: 4))
+let image = try await staticMap.render()
+
+try image.pngEncoded().write(to: URL(filePath: outputPath))
