38dd9bd8890ad10fb1e8eb0718d509c37473de67
fwcd <fwcd@fwcd.dev>
2024-09-28 04:22:55 +0200

    Stub out StaticMap.render

--- Sources/StaticMap/StaticMap+Render.swift
+++ Sources/StaticMap/StaticMap+Render.swift
@@ -1,7 +1,28 @@
 import CairoGraphics
+import Utils
 
 extension StaticMap {
-    public func render() -> CairoImage {
-        fatalError("TODO")
+    public func render() async throws -> CairoImage {
+        let mapRegion = MapRegion(self)
+        let tileRegion = TileRegion(mapRegion)
+
+        let image = try CairoImage(width: size.x, height: size.y)
+        let ctx = CairoContext(image: image)
+
+        try await withThrowingTaskGroup(of: CairoImage.self) { group in
+            for tile in tileRegion {
+                let url = tileProvider.url(for: tile)
+                group.addTask {
+                    // TODO: Are these guaranteed to be PNGs?
+                    try await HTTPRequest(url: url).fetchPNG()
+                }
+            }
+
+            for try await tileImage in group {
+                // TODO
+            }
+        }
+
+        return image
     }
 }
