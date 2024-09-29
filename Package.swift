d70ab44328977e83b3c85641ce556023efa459e6
fwcd <fwcd@fwcd.dev>
2024-09-29 05:54:09 +0200

    Add DocC plugin

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.1.0"),
         .package(url: "https://github.com/fwcd/swift-utils.git", from: "4.1.1"),
         .package(url: "https://github.com/fwcd/swift-geodesy.git", from: "0.2.8"),
         .package(url: "https://github.com/fwcd/swift-graphics.git", from: "4.0.0"),
