86f07efab5815f97a3548ee9146e76568f5ea234
fwcd <fwcd@fwcd.dev>
2024-09-29 05:15:18 +0200

    Fix TileRegion corners

--- Sources/StaticMap/TileRegion+MapRegion.swift
+++ Sources/StaticMap/TileRegion+MapRegion.swift
@@ -3,8 +3,8 @@ import Geodesy
 extension TileRegion {
     init(_ region: MapRegion) {
         self.init(
-            minPos: TileVec(region.coords.minCorner, zoom: region.zoom).rounded,
-            maxPos: TileVec(region.coords.maxCorner, zoom: region.zoom).rounded,
+            minPos: TileVec(region.coords.topLeft, zoom: region.zoom).rounded,
+            maxPos: TileVec(region.coords.bottomRight, zoom: region.zoom).rounded,
             zoom: region.zoom
         )
     }
