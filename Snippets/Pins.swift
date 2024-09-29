25891aee8b95a78b4810d90da109a8439c6ec7f4
fwcd <fwcd@fwcd.dev>
2024-09-29 23:25:04 +0200

    Update pins example

--- Snippets/Pins.swift
+++ Snippets/Pins.swift
@@ -12,7 +12,11 @@ let outputPath = CommandLine.arguments[1]
 let staticMap = StaticMap(
     center: .init(latitude: 51.5, longitude: 0),
     annotations: [
+        .pin(coords: .init(latitude: 51.499, longitude: -0.001)).color(.white),
+        .pin(coords: .init(latitude: 51.499, longitude: 0.001)).color(.blue),
         .pin(coords: .init(latitude: 51.5, longitude: 0)),
+        .pin(coords: .init(latitude: 51.501, longitude: -0.001)).color(.magenta),
+        .pin(coords: .init(latitude: 51.501, longitude: 0.001)).color(.gray),
     ]
 )
 let image = try await staticMap.render()
